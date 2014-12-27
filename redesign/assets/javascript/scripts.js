caf2af19cc5e35b0ba78db58174dfc4e94a2539f
nonlinear <nick@nonlinear.cc>
2014-12-26 20:21:39 -0500

    smoothstate

---
+++ redesign/assets/javascript/scripts.js
@@ -0,0 +1,35 @@
+// bootstrap tooltip and popover functions
+
+$('[data-toggle=tooltip]').tooltip();
+$('[data-toggle=popover]').popover();
+
+// flexslider functions
+
+// $(window).load(function(){
+//   $('.flexslider').flexslider({
+//     animation: "slide"
+//   });
+// });
+
+// smoothstate functions
+
+;(function($) {
+  'use strict';
+  var $body = $('html, body'),
+      content = $('#main').smoothState({
+        // Runs when a link has been activated
+        onStart: {
+          duration: 250, // Duration of our animation
+          render: function (url, $container) {
+            // toggleAnimationClass() is a public method
+            // for restarting css animations with a class
+            content.toggleAnimationClass('is-exiting');
+            // Scroll user to the top
+            $body.animate({
+              scrollTop: 0
+            });
+          }
+        }
+      }).data('smoothState');
+      //.data('smoothState') makes public methods available
+})(jQuery);
