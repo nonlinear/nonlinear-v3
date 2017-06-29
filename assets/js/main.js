d207fddf5c4082ee5762e562e5d59cf16e5c9719
Nicholas Frota <info@nicholasfrota.com>
2017-06-29 14:09:06 -0400

    no pursuance

--- assets/js/main.js
+++ assets/js/main.js
@@ -14190,46 +14190,22 @@ window.Modernizr = (function( window, document, undefined ) {
 
 })(this, this.document);
 
+// $(".red-toggle").click(function(){
+// });
 
-//toggle class loop: toggles <className> of all instances of <element>
-function toggleClass(element, className){
-    for (var i = 0; i < element.length; i++){
-        if (!element[i] || !className){
-            return;
-        }
-        
-        var classString = element[i].className, nameIndex = classString.indexOf(className);
-        if (nameIndex == -1) {
-            classString += ' ' + className;
-        }
-        else {
-            classString = classString.substr(0, nameIndex) + classString.substr(nameIndex+className.length);
-        }
-        element[i].className = classString;
-    }
-}
-//
-    var toggle = 'toggle';
-    var toggles = document.getElementsByClassName(toggle);
+$("[class$=-toggle]").click(function(event){
+    var prefixClass = event.target.className
+    var regex = "^[a-z0-9]*"
+    var prefix = prefixClass.match(regex)[0];
 
-for (var i = 0; i < toggles.length; i++){
-document.getElementsByClassName(toggle)[i].addEventListener('click', function() {
-    
+    var toggleActiveClassName = prefix + "-active";
 
-    var toggleName = this.className.match(/[^\s]*\b-toggle/);
-    var rootName = toggleName[0].slice(0, -7);
-    var target = rootName + '-target';
-    var active = rootName + '-active';
+    var toggleTargetClassName = prefix + "-target";
+    var toggleTargetDivName = "." + toggleTargetClassName;
 
-    var toggles = document.getElementsByClassName(toggleName);
-    var targets = document.getElementsByClassName(target);
-
-    toggleClass(toggles, active);
-    toggleClass(targets, active);
-
-
-});
-}
+    $(prefixClass).toggleClass(toggleActiveClassName);
+    $(toggleTargetDivName).toggleClass(toggleActiveClassName);
+})
 /*!
  * Detectizr v2.2.0
  * http://barisaydinoglu.github.com/Detectizr/
